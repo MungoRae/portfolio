1ee6bb989967d4dfdd35470bc6d9e0ef8bc387ad
Alex Macrae <alex@mungorae.me.uk>
2021-04-26 15:15:18 +0100

    Finished Me

--- sections/1_Me.js
+++ sections/1_Me.js
@@ -2,9 +2,20 @@ import React from "react";
 import Image from "next/image";
 import { Box, Grid, Typography } from "@material-ui/core";
 import Divider from "../components/Divider";
-import { LabelImportant } from "@material-ui/icons";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import { makeStyles } from "@material-ui/styles";
+
+const textItems = ["Android Expert", "iOS Developer", "Front End Specialist"];
+
+const useStyles = makeStyles((theme) => ({
+    textItem: {
+        textAlign: "center",
+    },
+}));
 
 const Me = () => {
+    const classes = useStyles();
+
     return (
         <Box
             component="header"
@@ -34,27 +45,38 @@ const Me = () => {
                         alignItems="center"
                         spacing={2}
                     >
-                        <Grid item>
-                            <Typography variant="h5">
-                                Mobile Developer
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <LabelImportant />
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h5">
-                                Front End Developer
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <LabelImportant />
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h5">
-                                Open Source Advocate
-                            </Typography>
-                        </Grid>
+                        {textItems.map((item) => (
+                            <Grid
+                                item
+                                xs={12}
+                                sm={12}
+                                md={12}
+                                lg={2}
+                                className={classes.textItem}
+                            >
+                                <Typography component="span" variant="h5">{item}</Typography>
+                                <Box component="span" ml={2}>
+                                    <CheckCircleOutlineIcon />
+                                </Box>
+                                {/* <Grid
+                                    container
+                                    direction="row"
+                                    alignItems="center"
+                                    justify="center"
+                                >
+                                    <Grid item>
+                                        <Typography variant="h5">
+                                            {item}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Box ml={2}>
+                                            <CheckCircleOutlineIcon />
+                                        </Box>
+                                    </Grid>
+                                </Grid> */}
+                            </Grid>
+                        ))}
                     </Grid>
                 </Grid>
             </Box>
